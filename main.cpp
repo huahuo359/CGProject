08f2cfba93a0b67a437c82b83e414c5d669d6185
huahuo359 <huahuo359@gmail.com>
2023-12-01 12:07:54 +0800

    存档

--- main.cpp
+++ main.cpp
@@ -175,7 +175,7 @@ public:
     GLuint TextureSpecular;
 
 
-    Car(): objModel("truck/monster truck 3d model.obj"), objShader("shaders/earth.vs", "shaders/earth.fs") {
+    Car(): objModel("truck/monster truck 3d model.obj"), objShader("shaders/car.vs", "shaders/car.fs") {
         
      
         std::cout << "load our car" << endl;
@@ -228,6 +228,123 @@ public:
 
 
         objModel.Draw(objShader);
+    }
+
+
+};
+
+
+
+
+std::vector<GLfloat> controlPoints = {
+    -1.5f, 0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+     0.5f, 0.5f, 0.0f,
+     1.5f, 0.5f, 0.0f,
+
+    -1.5f, 1.0f, 0.0f,
+    -0.5f, 1.0f, -1.0f,
+     0.5f, 1.0f, -1.0f,
+     1.5f, 1.0f, 0.0f,
+
+    -1.5f, 1.5f, 0.0f,
+    -0.5f, 1.5f, -2.0f,
+     0.5f, 1.5f, -2.0f,
+     1.5f, 1.5f, 0.0f,
+
+    -1.5f, 2.0f, 0.0f,
+    -0.5f, 2.0f, 0.0f,
+     0.5f, 2.0f, 0.0f,
+     1.5f, 2.0f, 0.0f,
+}; 
+
+class Bezier {
+public:
+
+
+    std::vector<float> points;
+    Shader shader;
+    unsigned int VBO, VAO;
+    GLuint EBO;
+
+    int Fact(int n) {
+        int res = 1;
+        if(n==0 || n==1) { return 1;}
+        
+        for(int i=1; i<=n; i++) {
+            res *= i;
+        }
+
+        return res;
+    }
+
+    int BinomialCoeff(int n, int i) {
+        return Fact(n)/(Fact(i)*Fact(n-i));
+    }
+
+    Bezier(): shader("shaders/bezier.vs", "shaders/bezier.fs") {
+        for(int i=0; i<400; ++i) {
+            float u = (float)i/400;
+            for(int j=0; j<400; ++j) {
+                float v = (float)j/400;
+                 // 计算二次贝塞尔曲面上的点
+                glm::vec3 point(0.0f, 0.0f, 0.0f);
+                for (int row = 0; row < 4; ++row) {
+                    float blendU = (float)BinomialCoeff(3, row) * glm::pow(1.0f - u, 3 - row) * glm::pow(u, row);
+                    for (int col = 0; col < 4; ++col) {
+                        
+                        float blendV = (float)BinomialCoeff(3, col) * glm::pow(1.0f - v, 3 - col) * glm::pow(v, col);
+                        point += blendU * blendV * glm::vec3(controlPoints[(row * 4 + col) * 3], controlPoints[(row * 4 + col) * 3 + 1], controlPoints[(row * 4 + col) * 3 + 2]);
+                }
+            }
+
+
+            points.push_back(point.x);
+            points.push_back(point.y);
+            points.push_back(point.z);
+
+
+            }
+        }
+
+        
+
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(float), &points[0], GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+    }
+
+    void Draw() {
+        shader.use();
+        shader.setInt("texture1", 1);
+       
+        glm::mat4 view;
+        view = glm::lookAt(camera.Position, camera.Position + camera.Front, camera.Up);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    
+    
+        shader.setMat4("projection", projection); 
+        shader.setMat4("view", view);
+        shader.setMat4("model", model);
+
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_POINTS, 0, points.size() / 3);
+        glBindVertexArray(0);
     }
 
 
@@ -618,14 +735,16 @@ void mainLoop(GLFWwindow* window ) {
    
    
     // SkyBox skybox1(faces);
-    Obj planet;
+    // Obj planet;
     // Car car;
     Planet sun("image/planet/sun.jpg", 2);
-    // Earth earth("image/planet/earth_diffuse.png", 1);
+    Earth earth("image/planet/earth_diffuse.png", 1);
     Moon moon("image/planet/earth_diffuse.png", 1);
-   
-    //bool flag = true;
-    bool flag = false;
+    // Bezier bezier;
+    bool flag = true;
+    //bool flag = false;
+
+    Bezier surface;
     
     while (!glfwWindowShouldClose(window))
     {
@@ -644,14 +763,17 @@ void mainLoop(GLFWwindow* window ) {
         if(flag) {
             // close shot
             //skybox1.Draw();
-            planet.Draw();
+            //planet.Draw();
             //car.Draw();
+            sun.Draw();
+            earth.Draw();
+            //surface.Draw();
 
         } else {
             // near shot
             //sun.Draw();
             // earth.Draw();
-            moon.Draw();
+            //moon.Draw();
         }
         
        
