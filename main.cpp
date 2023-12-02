b6e2147b53191d34935126994227f8407b75a443
huahuo359 <huahuo359@gmail.com>
2023-12-03 00:37:50 +0800

    add normal map and .dds texture loader

--- main.cpp
+++ main.cpp
@@ -45,6 +45,104 @@ glm::vec3 lightDir(-0.2f, -1.0f, -0.3f);
 glm::vec3 lightPos(0.0f, 0.0f, -10.0f);
 
 
+
+#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
+#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
+#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
+
+GLuint loadDDS(const char * imagepath)
+{
+    unsigned char header[124];
+
+    FILE *fp; 
+
+    /* try to open the file */ 
+    fp = fopen(imagepath, "rb"); 
+    if (fp == NULL)
+    {
+        printf("%s could not be opened. Are you in the right directory ? Don't forget to read the FAQ !\n", imagepath); getchar(); 
+        return 0;
+    }
+
+    /* verify the type of file */ 
+    char filecode[4]; 
+    fread(filecode, 1, 4, fp); 
+    if (strncmp(filecode, "DDS ", 4) != 0) 
+    {
+        fclose(fp); 
+        return 0; 
+    }
+
+    /* get the surface desc */ 
+    fread(&header, 124, 1, fp); 
+
+    unsigned int height      = *(unsigned int*)&(header[8 ]);
+    unsigned int width       = *(unsigned int*)&(header[12]);
+    unsigned int linearSize  = *(unsigned int*)&(header[16]);
+    unsigned int mipMapCount = *(unsigned int*)&(header[24]);
+    unsigned int fourCC      = *(unsigned int*)&(header[80]);
+
+    unsigned char * buffer;
+    unsigned int bufsize;
+    /* how big is it going to be including all mipmaps? */ 
+    bufsize = mipMapCount > 1 ? linearSize * 2 : linearSize; 
+    buffer = (unsigned char*)malloc(bufsize * sizeof(unsigned char)); 
+    fread(buffer, 1, bufsize, fp); 
+    /* close the file pointer */ 
+    fclose(fp);
+
+    unsigned int components  = (fourCC == FOURCC_DXT1) ? 3 : 4; 
+    unsigned int format;
+    switch(fourCC) 
+    { 
+    case FOURCC_DXT1: 
+        format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT; 
+        break; 
+    case FOURCC_DXT3: 
+        format = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT; 
+        break; 
+    case FOURCC_DXT5: 
+        format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT; 
+        break; 
+    default: 
+        free(buffer); 
+        return 0; 
+    }
+
+    // Create one OpenGL texture
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    // "Bind" the newly created texture : all future texture functions will modify this texture
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1);   
+
+    unsigned int blockSize = (format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT) ? 8 : 16; 
+    unsigned int offset = 0;
+
+    /* load the mipmaps */ 
+    for (unsigned int level = 0; level < mipMapCount && (width || height); ++level) 
+    { 
+        unsigned int size = ((width+3)/4)*((height+3)/4)*blockSize; 
+        glCompressedTexImage2D(GL_TEXTURE_2D, level, format, width, height,  
+            0, size, buffer + offset); 
+
+        offset += size; 
+        width  /= 2; 
+        height /= 2; 
+
+        // Deal with Non-Power-Of-Two textures. This code is not included in the webpage to reduce clutter.
+        if(width < 1) width = 1;
+        if(height < 1) height = 1;
+    } 
+
+    std::cout << "success to load DDS file" << std::endl;
+
+    free(buffer); 
+    return textureID;
+}
+
+
 GLFWwindow* Init()
 {
     glfwInit();
@@ -448,6 +546,201 @@ public:
 
 };
 
+class NormalEarth {
+    
+public:
+    Shader earthShader;
+    GLuint TextureDiffuse;
+    GLuint TextureSpecular;
+    GLuint TextureNormal;
+    std::vector<float> sphereVertices;  // 顶点属性
+    std::vector<int> sphereIndices;     // 顶点索引
+    int PRECISE = 64;
+    unsigned int VBO, VAO;
+    GLuint EBO;
+
+    NormalEarth(): earthShader("shaders/earth2.vs", "shaders/earth2.fs"){
+        // TextureDiffuse = loadTexture("image/planet/earth_diffuse.png", 1);
+        // TextureNormal = loadTexture("image/planet/earth_normal.png", 1);
+        // TextureSpecular = loadTexture("image/planet/earth_specular.png",1);
+
+        // TextureDiffuse = loadDDS("image/planet/earth_diffuse.dds");
+        // TextureNormal = loadDDS("image/planet/earth_normal.dds");
+        // TextureSpecular = loadDDS("image/planet/earth_specular.dds");
+
+        TextureDiffuse = loadDDS("image/planet/arctic_01_diffuse.dds");
+        TextureNormal = loadDDS("image/planet/arctic_01_normal.dds");
+        TextureSpecular = loadDDS("image/planet/arctic_01_specular.dds");
+
+
+
+        float PI = 3.14159;
+        // 生成球体的相关点数据
+        for(int i=0; i<=PRECISE; ++i) {
+            GLfloat pitch = -PI/2 + PI*i/PRECISE;
+            for(int j=0; j<=PRECISE; ++j) {
+                GLfloat yaw = 2*PI*j / PRECISE;
+
+                /* 下面有 11 个数据需要生成*/
+
+                // 生成顶点坐标 x,y,z 
+                GLfloat x = std::cos(pitch) * std::cos(yaw);
+                GLfloat y = std::sin(pitch);
+                GLfloat z = std::cos(pitch) * -std::sin(yaw); 
+
+                // 生成法向量 nx, ny, nz
+                GLfloat nx = std::cos(pitch) * std::cos(yaw);
+                GLfloat ny = std::sin(pitch);
+                GLfloat nz = std::cos(pitch) * -std::sin(yaw);
+
+                // 生成纹理坐标 ux, uy
+                GLfloat ux = (GLfloat)j/PRECISE;
+                GLfloat uy = (GLfloat)-i/PRECISE;
+
+                // 生成切线向量 fx, fy, fz
+                GLfloat fx = -std::sin(yaw);
+                GLfloat fy = 0.0f;
+                GLfloat fz = -std::cos(yaw);
+
+                sphereVertices.push_back(x);
+                sphereVertices.push_back(y);
+                sphereVertices.push_back(z);
+
+                sphereVertices.push_back(nx);
+                sphereVertices.push_back(ny);
+                sphereVertices.push_back(nz);
+
+                sphereVertices.push_back(ux);
+                sphereVertices.push_back(uy);
+
+                sphereVertices.push_back(fx);
+                sphereVertices.push_back(fy);
+                sphereVertices.push_back(fz);
+                
+            }
+        }
+
+
+        // 索引生成
+        for(int i=0; i<PRECISE; i++) {
+            for(int j=0; j<PRECISE; j++) {
+                sphereIndices.push_back(i * (PRECISE + 1) + j);
+                sphereIndices.push_back((i+1) * (PRECISE + 1) + j);
+                sphereIndices.push_back(i * (PRECISE + 1) + j + 1);
+                sphereIndices.push_back((i+1) * (PRECISE + 1) + j);
+                sphereIndices.push_back(i * (PRECISE + 1) + j + 1);
+                sphereIndices.push_back((i+1) * (PRECISE + 1) + j + 1);
+
+                // sphereIndices.push_back(i * (PRECISE + 1) + j);
+                // sphereIndices.push_back((i+1) * (PRECISE + 1) + j);
+                // sphereIndices.push_back((i+1) * (PRECISE + 1) + j + 1);
+                // sphereIndices.push_back(i * (PRECISE + 1) + j);
+                // sphereIndices.push_back((i+1) * (PRECISE + 1) + j + 1);
+                // sphereIndices.push_back(i * (PRECISE + 1) + j + 1);
+            }
+        }
+
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sphereVertices.size() * sizeof(float), &sphereVertices[0], GL_STATIC_DRAW);
+
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sphereIndices.size() * sizeof(int), &sphereIndices[0], GL_STATIC_DRAW);
+
+        // 绑定顶点坐标
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11*sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // 绑定法向量
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        // 绑定纹理坐标
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 11*sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        // 绑定切线向量
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 11*sizeof(float), (void*)(8 * sizeof(float)));
+        glEnableVertexAttribArray(3);
+
+        // unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+
+    }
+
+    void Draw() {
+        
+        earthShader.use();
+
+        
+        earthShader.setVec3("LightInfo.ambient", glm::vec3(0.05f, 0.05f, 0.05f)); 
+        earthShader.setVec3("LightInfo.diffuse", glm::vec3(1.0f, 1.0f, 1.0f));
+        earthShader.setVec3("LightInfo.specular", glm::vec3(0.5f, 0.5f, 0.5f));
+        earthShader.setVec3("viewPos", camera.Position);
+        
+        // set texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, TextureDiffuse);
+        earthShader.setInt("texture_diffuse", 0);
+
+        // set texture
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, TextureNormal);
+        earthShader.setInt("texture_normal", 1);
+
+        // set texture
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, TextureSpecular);
+        earthShader.setInt("texture_specular", 2);
+       
+        glm::mat4 view;
+        view = glm::lookAt(camera.Position, camera.Position + camera.Front, camera.Up);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
+        glm::mat4 model = glm::mat4(1.0f);
+
+        // float x_earth1 = 0 + 3*std::cos((GLfloat)glfwGetTime() * 0.5f);
+        // float z_earth1 = -10 + 3*std::sin((GLfloat)glfwGetTime() * 0.5f);
+        float x_earth1 = 0;
+        float z_earth1 = -4;
+        // model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+        model = glm::translate(model, glm::vec3( x_earth1,  0.0f, z_earth1));
+        
+        float angle = (GLfloat)glfwGetTime() * 3.5f;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.3f));
+
+        earthShader.setMat4("projection", projection); 
+        earthShader.setMat4("view", view);
+        earthShader.setMat4("model", model);
+
+        GLfloat lightX = 3*std::cos((GLfloat)glfwGetTime() * 0.1f);
+        GLfloat lightZ = 3*std::sin((GLfloat)glfwGetTime() * 0.1f);
+        earthShader.setVec3("lightPos", glm::vec3(x_earth1, 0.1f, 0.5f));
+        earthShader.setVec3("pointPose[0]", glm::vec3(x_earth1 - 2.0f, 1.1f, z_earth1 + 2.0f));
+        earthShader.setVec3("pointPose[1]", glm::vec3(x_earth1 + 2.0f, 1.1f, 0.5f));
+        earthShader.setVec3("pointPose[2]", glm::vec3(x_earth1+lightX , -2.1f, lightZ+z_earth1 - 3.0f));
+        earthShader.setVec3("pointPose[3]", glm::vec3(x_earth1, 2.1f, z_earth1 - 2.0f));
+
+
+        glm::vec3 earthPos = glm::vec3(x_earth1, 0.0f, z_earth1);
+        glm::vec3 sunPos = glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::vec3 lightDirection = -(earthPos - sunPos);
+        earthShader.setVec3("lightDir", lightDirection);
+
+
+
+        // glEnable(GL_CULL_FACE); 
+        // glCullFace(GL_BACK);
+        glBindVertexArray(VAO);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, PRECISE*PRECISE*6, GL_UNSIGNED_INT, 0);
+    }
+
+
+};
+
 class Earth : public Planet {
 public:
 
@@ -467,7 +760,7 @@ public:
         earthShader.setVec3("light.ambient", glm::vec3(0.05f, 0.05f, 0.05f)); 
         earthShader.setVec3("light.diffuse", glm::vec3(1.0f, 1.0f, 1.0f));
         earthShader.setVec3("light.specular", glm::vec3(0.5f, 0.5f, 0.5f));
-        // earthShader.setVec3("light.direction", glm::vec3(-0.2f, -1.0f, -0.3f));
+        earthShader.setVec3("light.direction", glm::vec3(-0.2f, -1.0f, -0.3f));
         earthShader.setVec3("viewPos", camera.Position);
         
         // set texture
@@ -512,8 +805,8 @@ public:
 
 
 
-        glEnable(GL_CULL_FACE); 
-        glCullFace(GL_BACK);
+       // glEnable(GL_CULL_FACE); 
+       //glCullFace(GL_BACK);
         glBindVertexArray(VAO);
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         glDrawElements(GL_TRIANGLES, PRECISE*PRECISE*6, GL_UNSIGNED_INT, 0);
@@ -737,14 +1030,17 @@ void mainLoop(GLFWwindow* window ) {
     // SkyBox skybox1(faces);
     // Obj planet;
     // Car car;
-    Planet sun("image/planet/sun.jpg", 2);
-    Earth earth("image/planet/earth_diffuse.png", 1);
-    Moon moon("image/planet/earth_diffuse.png", 1);
+    // Planet sun("image/planet/sun.jpg", 2);
+    // Earth earth("image/planet/earth_diffuse.png", 1);
+    // Moon moon("image/planet/earth_diffuse.png", 1);
     // Bezier bezier;
     bool flag = true;
     //bool flag = false;
 
     Bezier surface;
+
+    NormalEarth earth;
+    Earth earth2("image/planet/earth_diffuse.png", 1);
     
     while (!glfwWindowShouldClose(window))
     {
@@ -765,14 +1061,15 @@ void mainLoop(GLFWwindow* window ) {
             //skybox1.Draw();
             //planet.Draw();
             //car.Draw();
-            sun.Draw();
+            //sun.Draw();
             earth.Draw();
+            earth2.Draw();
             //surface.Draw();
 
         } else {
             // near shot
             //sun.Draw();
-            // earth.Draw();
+            earth.Draw();
             //moon.Draw();
         }
         
---
+++ shaders/earth2.vs
@@ -0,0 +1,44 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 anormal;
+layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in vec3 atangent;
+
+out vec2 TexCoord;
+out vec3 TangentViewPos;
+out vec3 TangentFragPos;
+out vec3 TangentLightPos;
+out vec3 TangentLightDir;
+out vec3 TangentPointPos[4];
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 viewPos;
+uniform vec3 lightPos;  // 点光源的坐标
+uniform vec3 lightDir;  // 设置一个平行光源
+uniform vec3 pointPose[4];   // 设定一组点光源
+
+void main()
+{
+	gl_Position = projection * view * model * vec4(aPos, 1.0);
+
+	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+
+    mat3 normalMat = transpose(inverse(mat3(model)));
+    vec3 T = normalize(normalMat * atangent);
+    vec3 N = normalize(normalMat * anormal);
+    vec3 B = normalize(cross(N, T));    // 通过切线和法向量计算副切线
+
+    mat3 TBN = transpose(mat3(T, B, N));
+
+    TangentViewPos = TBN * viewPos;
+    TangentFragPos = TBN * vec3(model * vec4(aPos, 1.0));
+    TangentLightPos = TBN * lightPos;
+    TangentLightDir = TBN * lightDir;
+
+    for(int i=0; i<4; i++)
+        TangentPointPos[i] = TBN * pointPose[i];
+
+	
+}
